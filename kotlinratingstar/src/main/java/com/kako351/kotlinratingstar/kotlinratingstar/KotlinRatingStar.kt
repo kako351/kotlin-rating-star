6096fbda1eefb6c0575bdbd161f1f2ee653c542b
kako351 <h3.birth@gmail.com>
2019-07-16 00:58:57 +0900

    add element

---
+++ kotlinratingstar/src/main/java/com/kako351/kotlinratingstar/kotlinratingstar/KotlinRatingStar.kt
@@ -0,0 +1,11 @@
+package com.kako351.kotlinratingstar.kotlinratingstar
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class KotlinRatingStar: ConstraintLayout {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+}
