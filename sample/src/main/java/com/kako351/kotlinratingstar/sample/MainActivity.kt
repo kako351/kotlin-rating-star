87dcc44898817aa347cc13c7a758eb807cc5c606
kako351 <h3.birth@gmail.com>
2019-07-21 18:03:40 +0900

    sample ratingChangeStar

--- sample/src/main/java/com/kako351/kotlinratingstar/sample/MainActivity.kt
+++ sample/src/main/java/com/kako351/kotlinratingstar/sample/MainActivity.kt
@@ -2,11 +2,18 @@ package com.kako351.kotlinratingstar.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kako351.kotlinratingstar.kotlinratingstar.KotlinRatingStar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ratingStar = this.findViewById<KotlinRatingStar>(R.id.ratingStar)
+        ratingStar.setRatingChangeListener {
+            Log.d("KotlinRatingStar", "${it}")
+        }
     }
 }
